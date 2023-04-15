0ce26c065a64d005cc45b3ca2462cecc5a1115e1
User44314 <user1234aaaa@gmail.com>
2023-04-15 19:03:36 +0300

    fead: additems binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,16 +6,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  root(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.root = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+    this.root = addWithin(this.root, value); // положи в наш корень то что вернет наша функция т.е какое то под дерево(this.root) некоторое значение (value)
+
+    function addWithin(node, value) {
+      if (!node) {
+        return new Node(value);
+      }
+
+      if (node.value === value) {
+        return node;
+      }
+
+      if (value < node.value) {
+        node.left = addWithin(node.left, value);
+      } else {
+        node.right = addWithin(node.right, value);
+      }
+
+      return node;
+    }
   }
 
   has(/* data */) {
@@ -47,3 +71,23 @@ class BinarySearchTree {
 module.exports = {
   BinarySearchTree
 };
+
+
+function addItems() {
+  console.log('\n  Add Items');
+  console.log('add 13, 15, 9, 20, 18, 32, 25');
+
+  bst.add(13);
+  bst.add(15);
+  bst.add(9);
+  bst.add(20);
+  bst.add(18);
+  bst.add(32);
+  bst.add(25);
+
+  console.log(bst);
+}
+
+const bst = new BinarySearchTree();
+
+addItems();
